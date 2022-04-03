9e897c145566f0a3094af3ef1c285549b028bdcd
tsukiy0 <tsukiy0@users.noreply.github.com>
2022-04-03 02:53:13 +0000

    chore: remove old web app

--- web/.eslintrc.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-
-module.exports = {
-  extends: ["@tsukiy0/eslint-config"],
-  parserOptions: {
-    project: path.resolve(__dirname, "tsconfig.json"),
-  }
-};
--- web/src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { AppProps } from "next/app";
-
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
-};
-
-export default MyApp;
